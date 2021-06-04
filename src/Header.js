7b0e7e03502f40262234defdc590212cc78b4caf
Harrf Akbar <harrf_akbar@yahoo.com>
2021-06-03 22:43:55 -0700

    work

--- src/Header.js
+++ src/Header.js
@@ -7,10 +7,12 @@ const Header = () => {
     return (
         <div className="ui secondary menu"> 
             <Link to="/" className="title item"> Home </Link>
-            <Link to="/about" className="title item"> About Me </Link>
-            <Link to="/projects" className="title item "> Projects </Link>
-            <Link to="/work" className="title item"> Work Experince </Link>
-            <Link to="/contact" className="title item"> Contact </Link>
+            <div className="right menu">
+                <Link to="/about" className="title item"> About Me </Link>
+                <Link to="/projects" className="title item "> Projects </Link>
+                <Link to="/work" className="title item"> Work Experince </Link>
+                <Link to="/contact" className="title item"> Contact </Link>
+            </div>
         </div>
     )
 }
