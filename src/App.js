ce19434eeb1eb6960a759af0bb2d61d36bae49eb
Harrf Akbar <harrf_akbar@yahoo.com>
2021-06-03 20:32:19 -0700

    created files and routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 
-
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Home from './Home'
+import About from './About'
+import Projects from './Projects'
+import Work from './Work'
+import Contact from './Contact'
 
 const App = () => {
 
     return (
-        <div>
-            App 
-        </div>
+        <>
+            <BrowserRouter>
+                <div>
+                    <div className=" main ui container">
+                        <Route path="/" exact component={Home} />
+                        <Route path="/about" exact component={About} />
+                        <Route path="/projects" exact component={Projects} />
+                        <Route path="/work" exact component={Work} /> 
+                        <Route path="/contact" exact component={Contact} /> 
+                    </div>
+                </div>
+            </BrowserRouter>
+        </>
     )
 }
 
